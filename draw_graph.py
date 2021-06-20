a0c7ac137064256fbfc8db18feba21147dd22559
kievking <jinlongren100@gmail.com>
2021-06-20 11:36:03 +0200

    Finishing up

--- draw_graph.py
+++ draw_graph.py
@@ -1,15 +1,23 @@
+from re import split
 from bokeh.plotting import figure, show, save
+
 
 # Example
 # prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+for filename in ['barometer_data.txt', 'fahrenheit_data.txt', 'humidity_data.txt']:
+  file1 = open(filename, 'r')
+  lines = file1.readlines()
+  y = []
+  for line in lines:
+    y.append(float(line.strip()))
+  x = range(1, len(lines)+1)
 
-# create a new plot with a title and axis labels
-p = figure(title="Simple line example", x_axis_label="x", y_axis_label="y")
 
-# add a line renderer with legend and line thickness
-p.line(x, y, legend_label="Temp.", line_width=2)
+  # create a new plot with a title and axis labels
+  p = figure(title=filename, x_axis_label="x", y_axis_label="y")
 
-# show the results
-save(p)
+  # add a line renderer with legend and line thickness
+  p.line(x, y, legend_label="Unit", line_width=2)
+
+  # show the results
+  save(p, filename=filename + '.html')
